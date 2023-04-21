2e13019990830952d2e23bae91b2d6fd5b3ddf91
JaakkoL <jaakko.lipponen01@gmail.com>
2023-04-21 14:43:17 +0300

    augh+ratio

--- app/src/main/java/com/kolmenHengenTyhma/ammattikissat/Battle.java
+++ app/src/main/java/com/kolmenHengenTyhma/ammattikissat/Battle.java
@@ -12,15 +12,14 @@ public class Battle {
     }
     public Cat battle(Cat c1, Cat c2){
         // TODO: battle logic? Make biased rng based on the attack of töiCissa?
-
         int RNGwinner = random.nextInt(2);
 
         if(RNGwinner == 0){
             // TODO: public atk ok?
-            c2.takeDMG(c1.attackPower-c2.defencePower);
+            c2.takeDMG(c1.getAttackPower()-c2.getDefencePower());
             return c1;
         }else{
-            c1.takeDMG(c2.attackPower-c1.defencePower);
+            c1.takeDMG(c2.getAttackPower()-c1.getDefencePower());
             return c2;
         }
     }
--- app/src/main/java/com/kolmenHengenTyhma/ammattikissat/Cat.java
+++ app/src/main/java/com/kolmenHengenTyhma/ammattikissat/Cat.java
@@ -5,24 +5,26 @@ import java.util.Random;
 
 public class Cat {
     // Attributes
-    public int attackPower;
-    public int defencePower;
+    private int attackPower;
+    private int defencePower;
     private String colour;
     private int currHP;
     private int maxHP;
-
     public double luck;
-    private ArrayList<String> statistics = new ArrayList<String>();
+    private ArrayList<String> matchHistory = new ArrayList<String>();
+    // Store matches in order with format "W"/"L"
+    private int trainedDays=0;
+
 
     //constructor
-    public Cat(String colour, int currHP, int maxHP, int attackPower, int defencePower, double luck, ArrayList<String> statistics) {
+    public Cat(String colour, int currHP, int maxHP, int attackPower, int defencePower, double luck, ArrayList<String> matchHistory) {
         this.colour = colour;
         this.currHP = currHP;
         this.maxHP = maxHP;
         this.attackPower = attackPower;
         this.defencePower = defencePower;
         this.luck = luck;
-        this.statistics = statistics;
+        this.matchHistory = matchHistory;
     }
     public void attackAbility(){
         // TODO: implement attack ability
@@ -31,11 +33,18 @@ public class Cat {
         currHP=maxHP;
     }
     public void takeDMG(int damage){
+        //TODO: LUCK?
         if(currHP > damage){
             currHP-=damage;
         }else if(currHP<=damage){
             // L cat (Dead 💀)
             currHP=0;
         }
+    }
+    public int getDefencePower() {
+        return defencePower;
+    }
+    public int getAttackPower() {
+        return attackPower;
     }
 }
