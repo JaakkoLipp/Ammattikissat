3851cc9b857922c32dfebe6b18ff37d69e7a8099
JaakkoL <jaakko.lipponen01@gmail.com>
2023-04-21 16:47:24 +0300

    mediaplayer+song in raw

--- app/src/main/java/com/kolmenHengenTyhma/ammattikissat/MainActivity.java
+++ app/src/main/java/com/kolmenHengenTyhma/ammattikissat/MainActivity.java
@@ -2,14 +2,19 @@ package com.kolmenHengenTyhma.ammattikissat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
+        mediaPlayer.start();
+
     }
 
 }
