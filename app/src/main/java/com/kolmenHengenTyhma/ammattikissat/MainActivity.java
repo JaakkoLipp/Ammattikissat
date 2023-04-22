a4aec033decb33093dd9f944daee06f829714252
JaakkoL <jaakko.lipponen01@gmail.com>
2023-04-22 12:57:30 +0300

    UI start screen creation

--- app/src/main/java/com/kolmenHengenTyhma/ammattikissat/MainActivity.java
+++ app/src/main/java/com/kolmenHengenTyhma/ammattikissat/MainActivity.java
@@ -12,9 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
-        mediaPlayer.start();
-
     }
-
 }
