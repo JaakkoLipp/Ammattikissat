8185444af12b0bc00f87366633b3386db4ce5b87
JaakkoL <jaakko.lipponen01@gmail.com>
2023-04-26 15:52:36 +0300

    Added two fragments, modified color scheme.

--- app/src/main/java/com/kolmenHengenTyhma/ammattikissat/HomeScreen.java
+++ app/src/main/java/com/kolmenHengenTyhma/ammattikissat/HomeScreen.java
@@ -1,8 +1,14 @@
 package com.kolmenHengenTyhma.ammattikissat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kolmenHengenTyhma.ammattikissat.fragments.CatRecycler;
+import com.kolmenHengenTyhma.ammattikissat.fragments.Information;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -10,7 +16,26 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
+        Button fragmentCRB = findViewById(R.id.catRecyclerButton);
+        Button fragmentInfoB = findViewById(R.id.informationButton);
+        fragmentCRB.setOnClickListener(listener);
+        fragmentInfoB.setOnClickListener(listener);
     }
-
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view){
+            Fragment fragment;
+            switch(view.getId()){
+                case R.id.catRecyclerButton:
+                    fragment = new CatRecycler();break;
+                case R.id.informationButton:
+                    fragment = new Information();break;
+                default:
+                    fragment = new CatRecycler();break;
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame, fragment)
+                    .commit();
+        }
+    };
 }
